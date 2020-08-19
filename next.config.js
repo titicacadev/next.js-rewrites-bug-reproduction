b005933f9f8824795cd68dc0ff08e002d60e6030
woody <woody@triple-corp.com>
2020-08-19 10:49:19 +0900

    Add API path to rewrites

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/docs/:slug',
-        destination: 'http://example.com/docs/:slug',
+        source: '/todos',
+        destination: `https://jsonplaceholder.typicode.com/todos`,
         basePath: false,
       },
     ]
